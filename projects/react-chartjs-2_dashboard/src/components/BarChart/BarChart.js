dca314d5265e5425ef5ab36d94e1a14c3a9e1ea6
Arkadiusz <a.konopka@selleo.com>
2022-03-11 10:40:13 +0100

    bars with total sale value of given product

--- projects/react-chartjs-2_dashboard/src/components/BarChart/BarChart.js
+++ projects/react-chartjs-2_dashboard/src/components/BarChart/BarChart.js
@@ -1,5 +1,74 @@
+import React, { useEffect, useState } from "react";
+import client from "../../axios";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import { uniq } from "lodash";
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: null,
+    title: {
+      display: true,
+      text: "Total sale of products",
+    },
+  },
+};
+
 function BarChart() {
-  return "Here goes bar chart";
+  const [data, setData] = useState([]);
+
+  const labels = data.map((label) => label.productType);
+  const topLabels = uniq(data?.map((element) => element.productType));
+  const barsData = data.map((element) => element.valueInDollars);
+
+  const chartDataTest = {
+    labels,
+    datasets: [
+      {
+        label: "",
+        data: barsData,
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
+
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  useEffect(() => {
+    client
+      .get(`/salesByCompanyProduct`)
+      .then((res) => setData(res.data))
+      .catch((err) => console.log("err", err));
+  }, []);
+
+  return (
+    <div
+      style={{
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Bar options={options} data={chartDataTest} />
+    </div>
+  );
 }
 
-export default BarChart
+export default BarChart;
