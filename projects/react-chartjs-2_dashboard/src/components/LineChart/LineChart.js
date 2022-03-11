28f7e660e991ce70fbcafc2d9d002517d60f2ef7
michalfirszt <michalfiszu@gmail.com>
2022-03-11 12:09:23 +0100

    Month names

--- projects/react-chartjs-2_dashboard/src/components/LineChart/LineChart.js
+++ projects/react-chartjs-2_dashboard/src/components/LineChart/LineChart.js
@@ -24,6 +24,7 @@ ChartJS.register(
     Legend
 );
 
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"]
 const monthLabels = ['-01-', '-02-', '-03-', '-04-', '-05-', '-06-', '-07-', '-08-', '-09-', '-10-', '-11-', '-12-']
 
 const data = {
@@ -76,7 +77,7 @@ function LineChart() {
           <select onChange={(event) => setPageIndex(Number(event.target.value))}>
             {range(12).map((option) => (
                 <option key={option} value={option}>
-                  {option + 1}
+                  {months[option]}
                 </option>
             ))}
           </select>
