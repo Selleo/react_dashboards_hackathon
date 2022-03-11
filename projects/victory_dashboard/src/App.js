18f8c27701f7a181d8eb62cfd8e1fe4e13453f0f
Anna-Bala <a.bala@selleo.com>
2022-03-11 12:54:48 +0100

    Add line chart

--- projects/victory_dashboard/src/App.js
+++ projects/victory_dashboard/src/App.js
@@ -11,23 +11,13 @@ function App() {
   return (
     <div className="bg-slate-200 h-screen">
       <div className="container mx-auto py-6 grid grid-cols-12 gap-6 h-full">
-        <div className={cn("col-span-full", cardStyles)}>
+        <div className={cn("col-span-6 ", cardStyles)}>
           <LineChart />
         </div>
-        <div
-          className={cn(
-            "col-span-10 col-start-2 md:col-start-1 md:col-span-6",
-            cardStyles
-          )}
-        >
+        <div className={cn("col-span-10 col-start-2 md:col-start-1 md:col-span-6", cardStyles)}>
           <BarChart />
         </div>
-        <div
-          className={cn(
-            "col-span-10 col-start-2 md:col-start-7 md:col-span-6",
-            cardStyles
-          )}
-        >
+        <div className={cn("col-span-10 col-start-2 md:col-start-7 md:col-span-6", cardStyles)}>
           <PieChart />
         </div>
       </div>
